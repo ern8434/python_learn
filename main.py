30d136b3a2c3d1165bfa8b683bdcb0b764b7e087
Eren Ezgü <erenezgu@gmail.com>
2025-12-13 14:50:11 +0300

    basic string operations

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-# Created on : 2 Ara 2025, 11:58:07
-# Author     : eren
+
+word = 'Python'
+print('word '+ word)
+print('word[0] ' + word[0])
+print('word[-1] ' + word[-1])
+
+print('word[0:2] ' + word[0:2])
+
+print('word[:2] ' + word[:2])
+print('word[4:] ' + word[4:])
+
+#  +---+---+---+---+---+---+
+#  | P | y | t | h | o | n |
+#  +---+---+---+---+---+---+
+#  0   1   2   3   4   5   6
+# -6  -5  -4  -3  -2  -1
 
 
-# This is a sample Python script.
-
-# Press Shift+F6 to execute it or replace it with your code.
-
-
+print('len(word):')
+print(len(word))

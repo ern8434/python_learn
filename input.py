d997c4e115edb5ac80b6c6910902c77a1c7af219
Eren Ezgü <erenezgu@gmail.com>
2026-01-14 14:07:07 +0300

    ..

---
+++ input.py
@@ -0,0 +1,7 @@
+
+x = float(input("toplam için iki birinci sayı:"))
+y = float(input("toplam için iki ikinci sayı:"))
+
+print("Toplam = "+ str(x+y))
+
+
---
+++ math.py
@@ -0,0 +1,27 @@
+
+import math
+
+# x = 8
+# y = 5
+
+x = float(input("x:"))
+y = float(input("y:"))
+
+print(x+y)
+
+print(x/y) # float
+print(x//y) # integer
+print(x%y) # mod
+
+print(x**y) # power
+
+round(3.14)
+
+print(abs(-3.14))
+
+print( math.sqrt(16) )
+
+print( min(4,7,12,2) )
+
+print( max(4,7,12,2) )
+

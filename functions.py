23ff8c9e1e2fff170e7e40d70896a00b6934eddb
Eren Ezgü <erenezgu@gmail.com>
2026-01-18 15:26:01 +0300

    ..

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 
+# açıklamalı fonksiyon ( help(f) ile açıklama gösterilir )
 def f(x):
     '''gelen sayıya 5 ekler'''
     y= x+5
     return y
 
 print(f(3))
-
 
 
 
@@ -19,3 +19,15 @@ print( kokAl(9) )
 print( kokAl(125,3) )
 
 
+
+# çoklu sonuç döndürme
+def cember(r):
+    c=2*3.14*r
+    a=3.14*(r**2)
+    return c,a
+
+print(cember(4)) # tuple türünde
+
+cevre,alan = cember(4)
+print(cevre)
+print(alan)
---
+++ global.py
@@ -0,0 +1,9 @@
+def deneme():
+    global y
+    print(y+10)
+    y=90
+    print(y*3)
+
+y= 20
+deneme()
+print(y)

9503af5f1ba87f5f9a2934c22ff7f3ef2585d8ea
Eren Ezgü <erenezgu@gmail.com>
2026-03-14 15:09:24 +0300

    dict

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+
+sozluk = {
+    'one':'bir',
+    'two':'iki'
+}
+
+sozluk['key'] = 'anahtar'
+sozluk['value'] = 'değer'
+
+print(sozluk)
+
+if 'key' in sozluk:
+    print(sozluk['key'])
+else:
+    print('sözlükte yok')
+
+
+
+
